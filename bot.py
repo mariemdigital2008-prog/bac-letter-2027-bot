3c484bb2f70e859447bc4aa3e381bd468268031f
mariemdigital2008-prog <mariemdigital2008@gmail.com>
2026-08-28 07:50:09 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,23 @@
+import os
+from telegram import Update
+from telegram.ext import Application, CommandHandler, ContextTypes
 
+TOKEN = os.getenv("BOT_TOKEN")
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text(
+        "🇹🇳 عسلامة! أنا بوت Bac Lettres 2027 📚\n\n"
+        "البوت متاعي مازال في مرحلة التجربة، "
+        "وقريب نبدأ نخدم معاكم ❤️"
+    )
+
+def main():
+    app = Application.builder().token(TOKEN).build()
+
+    app.add_handler(CommandHandler("start", start))
+
+    print("Bot is running...")
+    app.run_polling()
+
+if __name__ == "__main__":
+    main()
